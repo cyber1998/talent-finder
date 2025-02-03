9c45e2770e6ad4251aef3590fafc8ac0126226ea
Cyber <cybr1998@gmail.com>
2025-02-03 11:03:13 +0530

    feat: add the resume type to be returned

--- talent/graphql/types/__init__.py
+++ talent/graphql/types/__init__.py
@@ -1,3 +1,5 @@
+import graphene
+from graphene import ObjectType
 from graphene_django import DjangoObjectType
 
 from talent.models import Talent
@@ -6,3 +8,19 @@ from talent.models import Talent
 class TalentType(DjangoObjectType):
     class Meta:
         model = Talent
+
+
+class ResumeType(ObjectType):
+    first_name = graphene.String()
+    last_name = graphene.String()
+    email = graphene.String()
+
+    def resolve_first_name(self, info):
+        return self.first_name
+
+    def resolve_last_name(self, info):
+        return self.last_name
+
+    def resolve_email(self, info):
+        return self.email
+
